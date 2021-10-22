856103a5e8d05bc42518d6b48802e7c1055db707
syacooms <syacooms@gmail.com>
2021-10-22 22:36:49 +0900

    Add 메세지 수정, 삭제

--- client/components/MsgItem.js
+++ client/components/MsgItem.js
@@ -1,4 +1,6 @@
-const MsgItem = ({userId,timestamp,text}) => (
+import MsgInput from "./MsgInput"
+
+const MsgItem = ({ id, userId, timestamp, text, onUpdate, isEditing, startEdit, onDelete }) => (
     <li className="messages__item">
         <h3>
             {userId}{' '}
@@ -13,7 +15,16 @@ const MsgItem = ({userId,timestamp,text}) => (
                 })}
             </sub>
         </h3>
-        {text}
+        {isEditing ? (
+            <>
+                <MsgInput mutate={onUpdate} text={text} id={id} />
+            </>
+        ) : (text)}
+
+        <div className="messages__buttons">
+            <button onClick={startEdit}>수정</button>
+            <button onClick={onDelete}>삭제</button>
+        </div>
     </li>
 )
 
--- client/components/MsgList.js
+++ client/components/MsgList.js
@@ -24,6 +24,7 @@ const originalMsgs = Array(50).fill(0).map((_, i) => ({
 
 const MsgList = () => {
     const [msgs, setMsgs] = useState(originalMsgs)
+    const [editingId, setEditingId] = useState(null)
 
     const onCreate = text => {
         const newMsg = {
@@ -35,11 +36,43 @@ const MsgList = () => {
         setMsgs(msgs => ([newMsg, ...msgs]))
         //console.log(msgs);
     }
+
+    const onUpdate = (text, id) => {
+        setMsgs(msgs => {
+            const targetIndex = msgs.findIndex(msg => msg.id === id)
+            if (targetIndex < 0) return msgs;
+            const newMsgs = [...msgs]
+            newMsgs.splice(targetIndex, 1, {
+                ...msgs[targetIndex],
+                text
+            })
+            return newMsgs
+        })
+        doneEdit();
+    }
+
+    const onDelete = (id) => {
+        setMsgs(msgs => {
+            const targetIndex = msgs.findIndex(msg => msg.id === id)
+            if (targetIndex < 0) return msgs;
+            const newMsgs = [...msgs]
+            newMsgs.splice(targetIndex, 1)
+            return newMsgs
+        })
+    }
+
+    const doneEdit = () => setEditingId(null);
+
     return (
         <>
             <MsgInput mutate={onCreate} />
             <ul className="messages">
-                {msgs.map(x => (<MsgItem key={x.id} {...x} />))}
+                {msgs.map(x => (<MsgItem key={x.id} 
+                onUpdate={onUpdate} 
+                onDelete={() => onDelete(x.id)}
+                startEdit={() => setEditingId(x.id)} 
+                isEditing={editingId === x.id} 
+                {...x} />))}
             </ul>
         </>
     )
